6e524c1b1e5e83b764be298fd170b8eddd955402
br <brrrgpu@gmail.com>
2024-03-19 11:54:35 -0600

    Tercer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\RoboticsKitController;
+use App\Http\Controllers\DidacticMaterialController;
 
+Route::resource('users', UserController::class);
+Route::resource('roles', RoleController::class);
+Route::resource('groups', GroupController::class);
+Route::resource('courses', CourseController::class);
+Route::resource('robotics-kits', RoboticsKitController::class);
+Route::resource('didactic-materials', DidacticMaterialController::class);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
